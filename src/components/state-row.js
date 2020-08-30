a39ba1d1bf6c7bcdf1e9d70ba4f0e5877e1c690e
Dean Putney <dean@glowforge.com>
2020-08-30 15:01:31 -0700

    Move short bar labels outside of bar

--- src/components/state-row.js
+++ src/components/state-row.js
@@ -14,10 +14,21 @@ const StateRow = (data) => {
   // The first colspan is how far from left to offset the start of the bar.
   // This corresponds to how many days from TODAY is the beginning of the voting period.
   // The second colspan is how long the voting period is.
+
+  if (data.daysToVote < 3) {
+    return (
+      <tr>
+        <th>{data.state}</th>
+        <td colspan={data.daysToStart} class="bar-spacer">{data.shortStartDate}</td>
+        <td colspan={data.daysToVote} class="bar"></td>
+      </tr>
+    )
+  }
+
   return (
     <tr>
       <th>{data.state}</th>
-      <td colspan={data.daysToStart}></td>
+      <td colspan={data.daysToStart} class="bar-spacer"></td>
       <td colspan={data.daysToVote} class="bar">{data.shortStartDate}</td>
     </tr>
   )
